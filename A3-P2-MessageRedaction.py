c0ebd98d18751c0ba28ec188f81620613b85daf0
Dan Shaw <w0190983@nscc.ca>
2024-11-15 09:09:37 -0400

    Comments and documentation

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -8,32 +8,43 @@ import ds_tower1_3_0 as tower
 
 def main():
     title = "Message Redaction Program"
+    firstRun = True
+    inputString = ""
+    # Outputs formatted title
     print(tower.Template.titleOut(title))
 
-    inputString = ""
-
-    while True:
-        if (inputString := getInputString()).upper() == "QUIT":
+    while True: # Run the loop until the user enters 'quit' or 'q'. I chose not to manually break the loop instead of putting the condition in the declaration because if the user entered 'quit' the loop would finish running before quitting
+        if (inputString := getInputString(firstRun)).upper() == "QUIT" or inputString.upper() == "Q":
             break
 
         lettersToRedact = getLettersToRedact()
 
+        # Iterate through the inputString and keep a count of every character replaced for output later
         count = 0
         for c in inputString:
             if c.upper() in lettersToRedact:
                 inputString = inputString.replace(c, "_")
                 count += 1
 
-        print("Number of letters redacted: " + str(count))
         print("Redacted message: " + inputString)
+        print("Number of letters redacted: " + str(count))
+        firstRun = False
     
     print("Thank you for using the Message Redaction Program")
     print("Goodbye.")
 
-def getInputString():
-    return input("Enter a sentence or phrase (Quit to exit program): ")
+def getInputString(firstRun:bool):
+    """ Gets the input string from the user. On subsequent runs, the instruction will change. """
+
+    if firstRun == True:
+        instr = "Enter a sentence or phrase (Quit to exit program): "
+    else:
+        instr = "Enter another sentence or phrase (Quit to exit program): "
+    return input(instr)
 
 def getLettersToRedact():
+    """ Gets the letters to redact from the user. """
+
     print("Enter a comma-separate list of letters to redact")
     while (lettersToRedact := validateCharacterList(input("> ").split(","))) == None:
         print("Invalid input\nEnter a comma-separate list of letters to redact")
@@ -43,7 +54,7 @@ def getLettersToRedact():
 def validateCharacterList(list):
     """ Validates whether list is a valid list of characters. Returns a list of characters if successful. Returns None if fails. """
     for i in range(len(list)):
-        list[i] = list[i].strip()
+        list[i] = list[i].strip() # Removes whitespace. This allows the user to use spaces in their comma-separated list
         list[i] = list[i].upper()
         if len(list[i]) != 1 or not list[i].isalpha(): # If the resultant string is more than one character, or not a letter, return None
             return None
